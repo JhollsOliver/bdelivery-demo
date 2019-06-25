10a0100cd892d2237a2ee135223f37f312a300f2
Giovanne <giovanne@pm.me>
2019-06-25 16:03:12 -0300

    Deactivate Debug

--- www/js/config.js
+++ www/js/config.js
@@ -2,5 +2,5 @@ var krms_config ={
 	'ApiUrl':"https://bdelivery.gq/singlemerchant/api",
 	'DialogDefaultTitle':"Demo",
 	'MerchantKeys':"6c2a62cfb0ef420cfe433a00237c8720",
-	'debug': true
+	'debug': false
 };

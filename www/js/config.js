de5cc313734b4957bd2f322d2dfa25cfe211bdf4
Giovanne <giovanne@pm.me>
2019-11-05 09:51:44 -0300

    New Theme

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,5 @@
 var krms_config ={			
-	'ApiUrl':"https://bdelivery.gq/singlemerchant/api",
+	'ApiUrl':"https://delivery.giovanne.tech/singlemerchant/api",
 	'DialogDefaultTitle':"Demo",
 	'MerchantKeys':"6c2a62cfb0ef420cfe433a00237c8720",
 	'debug': false
